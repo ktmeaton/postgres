4636d232ee7c21b0d0a1a17143352cdf53ad1477
Katherine Eaton <ktmeaton@gmail.com>
2025-06-19 14:52:05 -0600

    scripts: generate add echo extended

--- scripts/generate_credentials.sh
+++ scripts/generate_credentials.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 {
-    echo "# Host User information for non-root container usage"
+    echo -e "# Host User information for non-root container usage"
     echo USER_ID=$(id -u)
     echo GROUP_ID=$(id -g)
     echo USER_GROUP_ID=$(id -u):$(id -g)
 
-    echo "\n# Custom Credentials"
+    echo -e "\n# Custom Credentials"
     echo POSTGRES_USER=postgres
     echo POSTGRES_PASSWORD=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | head -c 50)
 
-    echo "\n# Custom Credentials"
+    echo -e "\n# Custom Credentials"
     echo CUSTOM_USER=custom
     echo CUSTOM_PASSWORD=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | head -c 50)
     echo CUSTOM_DB=custom  
