8bff3e0fc15cd3144937a092b5124c6ee0c75e92
Katherine Eaton <ktmeaton@gmail.com>
2025-07-10 11:59:07 -0600

    cron: simplify backup schedule

--- scripts/sql/setup/cron.sql
+++ scripts/sql/setup/cron.sql
@@ -14,13 +14,13 @@ comment on table cron.job_run_details is 'job run details scheduled with pg_cron
 -- to unschedule is: select cron.unschedule(1);
 -- to schedule is: select cron.schedule_in_database('<name of the scheduled job>', '<schedule>', '<job content>', '<database>', '<database account>', '<enable the job>');
 
-\echo 'scheduling job: name=backup_full, frequency=11 pm on saturdays'
-select cron.schedule_in_database('backup_full', '0 23 * * sat', 'select backup.run(''full'', ''source="cron"'')', 'postgres', 'postgres', true);
+\echo 'scheduling job: name=backup_full, frequency=midnight on sundays'
+select cron.schedule_in_database('backup_full', '0 0 * * sun', 'select backup.run(''full'', ''source="cron"''); select backup.run(''diff'', ''source="cron"'');', 'postgres', 'postgres', true);
 
-\echo 'scheduling job: name=backup_diff, frequency=11 pm on sunday to friday'
-select cron.schedule_in_database('backup_diff', '0 23 * * sun-fri', 'select backup.run(''diff'', ''source="cron"'')', 'postgres', 'postgres', true);
+\echo 'scheduling job: name=backup_diff, frequency=midnight on sunday to friday'
+select cron.schedule_in_database('backup_diff', '0 0 * * sun-fri', 'select backup.run(''diff'', ''source="cron"'')', 'postgres', 'postgres', true);
 
-\echo 'scheduling job: name=backup_diff, frequency=hourly'
-select cron.schedule_in_database('backup_incremental', '0 * * * *', 'select backup.run(''incr'', ''source="cron"'')', 'postgres', 'postgres', true);
+\echo 'scheduling job: name=backup_diff, frequency=every 6 hours'
+select cron.schedule_in_database('backup_incremental', '0 */6 * * *', 'select backup.run(''incr'', ''source="cron"'')', 'postgres', 'postgres', true);
 
 select * from cron.job;
