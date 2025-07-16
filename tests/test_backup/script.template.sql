bfa819a63cd7b27fd35165d2c781683d21986f85
Katherine Eaton <ktmeaton@gmail.com>
2025-07-16 16:12:59 -0600

    tests: pause scheduled backups while running

--- tests/test_backup/script.template.sql
+++ tests/test_backup/script.template.sql
@@ -3,6 +3,9 @@
 create database {DB};
 \c {DB}
 
+\echo 'Pausing scheduled backups'
+update timetable.chain set live=false where chain_name like 'backup%';
+
 drop table if exists test;
 create table test (id integer, name text);
 select pg_sleep(1);
@@ -28,3 +31,6 @@ insert into test values (3, 'C');
 select pg_sleep(1);
 \c postgres
 select backup.run('incr', 'source="test_backup" --annotation=comment="after_final"');
+
+\echo 'Resuming scheduled backups'
+update timetable.chain set live=true where chain_name like 'backup%';
