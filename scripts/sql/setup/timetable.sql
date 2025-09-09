f0e7675bb2f0f8e765f58e45b93aa95c871850c0
Katherine Eaton <ktmeaton@gmail.com>
2025-09-09 10:22:50 -0600

    extension: update job scheduling for timetable

--- scripts/sql/setup/timetable.sql
+++ scripts/sql/setup/timetable.sql
@@ -6,30 +6,51 @@
 -- setup cron extension, it must be located in the postgres database.
 comment on schema timetable is 'Job scheduling with pg_timetable.';
 
-\echo 'scheduling job: name=backup_full, frequency=midnight on sundays'
-SELECT timetable.add_job(
-    job_name            => 'backup_full',
-    job_schedule        => '0 0 * * 0',
-    job_command         => 'select backup.run(''full'', ''source="pg_timetable"''); select backup.run(''diff'', ''source="pg_timetable"'')',
-    job_max_instances   => 1
-);
+do $$
+declare
+    backups   text[] := '{
+        full,
+        diff,
+        incr
+    }';
+    -- Every sunday at midnight
+    -- Every 2 hours at 1am (01:00) and 1pm (13:00)
+    -- Every 6 hours, at 2am, 8am, 2pm, 8pm
+    schedules text[] := '{
+        "0 0 * * 0",
+        "0 1,13 * * *",
+        "0 2,8,14,20 * * *"
+    }';
+    commands  text[] := '{
+        "select backup.run(''full'', ''source=\"pg_timetable\"'')",
+        "select backup.run(''diff'', ''source=\"pg_timetable\"'')",
+        "select backup.run(''incr'', ''source=\"pg_timetable\"'')"
+    }';
+    backup    text;
+    name      text;
+    schedule  text;
+    command   text;
+begin
 
--- Every 2 hours at 1am (01:00) and 1pm (13:00)
-\echo 'scheduling job: name=backup_diff, frequency=every 12 hours'
-SELECT timetable.add_job(
-    job_name            => 'backup_diff',
-    job_schedule        => '0 1,13 * * *',
-    job_command         => 'select backup.run(''diff'', ''source="pg_timetable"'')',
-    job_max_instances   => 1
-);
+    FOR i IN 1 .. array_length(backups, 1) loop
+        backup := backups[i];
+        name := 'backup_' || backup;
+        schedule := schedules[i];
+        command  := commands[i];
 
--- Every 6 hours, at 2am, 8am, 2pm, 8pm
-\echo 'scheduling job: name=backup_incr, frequency=every 2 hours'
-SELECT timetable.add_job(
-    job_name            => 'backup_incr',
-    job_schedule        => '0 2,8,14,20 * * *',
-    job_command         => 'select backup.run(''incr'', ''source="pg_timetable"'')',
-    job_max_instances   => 1
-);
+        if not exists (select from timetable.chain where chain_name = name and run_at = schedule) then
+            raise notice 'scheduling job: name=%, schedule=%', name, schedule;
+            delete from timetable.chain where chain_name = name;
+            PERFORM timetable.add_job(
+                job_name            => name,
+                job_schedule        => schedule,
+                job_command         => command,
+                job_max_instances   => 1
+            );
+        else
+            raise notice 'job is already scheduled: name=%, schedule=%', name, schedule;
+        end if;
+    end loop;
+end $$;
 
 select * from timetable.chain;
